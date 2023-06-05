58f49362d8fa40dc8bd8e7b0c154b04d8cd69fd4
Rhys <rhys.e.sheehan@protonmail.com>
2023-06-05 19:46:32 +1000

    Add loop to count in 10s from 0 to 100

--- Practicals/Prac 01/loops.py
+++ Practicals/Prac 01/loops.py
@@ -1,9 +1,12 @@
 """CP1404 Rhys Sheehan Prac 01 loops """
 
-for i in range(1, 21, 2):
+# for i in range(1, 21, 2):
+#     print(i, end=' ')
+# print()
+
+for i in range(0, 101, 10):
     print(i, end=' ')
 print()
-
 
 
 
