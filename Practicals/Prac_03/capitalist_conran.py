63d5c5f1807cf1ef4016a3f3fc6f5078dfd898fe
Rhys <rhys.e.sheehan@protonmail.com>
2023-06-18 10:41:06 +1000

    Add counter to count the number of days the progam has simulated

--- Practicals/Prac_03/capitalist_conran.py
+++ Practicals/Prac_03/capitalist_conran.py
@@ -14,12 +14,14 @@ MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 0.01
 MAX_PRICE = 1000.0
 INITIAL_PRICE = 10.0
+DAY_NUMBER = 0
 
 price = INITIAL_PRICE
 print(f"${price:,.2f}")
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
+    DAY_NUMBER += 1
     # generate a random integer of 1 or 2
     # if it's 1, the price increases, otherwise it decreases
     if random.randint(1, 2) == 1:
@@ -32,4 +34,4 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+    print(f"On day {DAY_NUMBER} price is ${price:,.2f}")
