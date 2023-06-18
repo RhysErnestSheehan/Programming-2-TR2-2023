60d63c99fef14cf0d608cc40702bf6d50f2d981d
Rhys <rhys.e.sheehan@protonmail.com>
2023-06-18 10:21:20 +1000

    Add answers to questions from practical 3 randoms.py

---
+++ Practicals/Prac_03/randoms.py
@@ -0,0 +1,24 @@
+# import random
+#
+# print(random.randint(5, 20))  # line 1
+# print(random.randrange(3, 10, 2))  # line 2
+# print(random.uniform(2.5, 5.5))  # line 3
+
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+# I saw it print a random number between five and twenty.
+# The smallest number I could of seen is five and the largest is twenty.
+
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+# I saw a number between three and 10 being printed, skipping a number each time.
+#
+# Line two could not have produced a four because it started at three and skipped a number each time.
+
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+# I saw line three print a floating point number between 2.5 and 5.5.
+# The smallest number I could of seen was 2.5 and the largest was 5.5.
+
+
