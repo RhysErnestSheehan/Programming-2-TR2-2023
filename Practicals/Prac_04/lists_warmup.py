dde07919a975a047cfed14e886420792de57f510
Rhys <rhys.e.sheehan@protonmail.com>
2023-06-22 15:34:43 +1000

    Add answers to questions from lists_warmup question from CP1404

---
+++ Practicals/Prac_04/lists_warmup.py
@@ -0,0 +1,24 @@
+"""Rhys Sheehan CP1404 Prac 04 lists warmup"""
+
+# numbers = [3, 1, 4, 1, 5, 9, 2]
+# numbers[0]
+# This will print the first value in the list.
+# numbers[-1]
+# This will print the last value in the list.
+# In this case it will be the number two.
+# numbers[3]
+# This will print the fourth value in the list
+# numbers[:-1]
+# This will slice the list to remove the last value.
+# numbers[3:4]
+# This will remove the first three numbers and the
+# last four number from the fourth number onward
+# 5 in numbers
+# True
+# 7 in numbers
+# False
+# "3" in numbers
+# False
+# numbers + [6, 5, 3]
+# This will add the numbers six, five and three to the numbers list
+
