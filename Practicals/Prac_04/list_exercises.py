549b0a3e03e02f94c9a8964d4559c759bfa305ac
Rhys <rhys.e.sheehan@protonmail.com>
2023-06-24 16:41:15 +1000

    Add print statements to display information about the numbers list

--- Practicals/Prac_04/list_exercises.py
+++ Practicals/Prac_04/list_exercises.py
@@ -4,3 +4,7 @@ numbers = []
 for i in range(5):
     numbers_picked = int(input("Number: "))
     numbers.append(numbers_picked)
+print(f"The first number is {numbers[0]}.")
+print(f"The last number is {numbers[4]}.")
+print(f"The smallest number is {min(numbers)}.")
+print(f"The largest number is {max(numbers)}.")
