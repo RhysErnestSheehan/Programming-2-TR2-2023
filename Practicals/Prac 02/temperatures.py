db09df62541c461c9e0160815ccc03b37ed7d8c3
Rhys <rhys.e.sheehan@protonmail.com>
2023-07-22 11:14:53 +1000

    Add program to test ProgramingLanguage class

---
+++ Practicals/Prac 02/temperatures.py
@@ -0,0 +1,17 @@
+"""Rhys Sheehan CP1404 Temperatures"""
+
+
+def main():
+    pass
+
+
+def celsius_to_fahrenheit():
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
+def fahrenheit_to_celsius():
+    pass
+
+
+main()
